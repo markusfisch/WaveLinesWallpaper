192cebdf3af455de6ffea5162c2dfcf8317f3aa9
Markus Fisch <mf@markusfisch.de>
2019-07-26 19:23:19 +0200

    Hide status and navigation bar in preview activity
    Requires another tap to return from it but I think it's
    better to have it clear and undisturbed.

--- app/src/main/java/de/markusfisch/android/wavelines/activity/PreviewActivity.java
+++ app/src/main/java/de/markusfisch/android/wavelines/activity/PreviewActivity.java
@@ -44,7 +44,9 @@ public class PreviewActivity extends AppCompatActivity {
 		window.getDecorView().setSystemUiVisibility(
 				View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
 						View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-						View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+						View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+						View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+						View.SYSTEM_UI_FLAG_FULLSCREEN);
 		window.setStatusBarColor(0);
 		window.setNavigationBarColor(0);
 	}
